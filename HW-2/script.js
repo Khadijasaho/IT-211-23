1069aaf94a56d19a71d9640f23fa05192d65450b
Khadija Saho <sahokhadija444@gmail.com>
2023-07-07 22:47:32 -0700

    add text

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,21 +1,19 @@
-function toPigLatin(str) {
-    if (!str || str.length === 1) {
-        return "Input can't be translated";
-    }
-    
-    const vowels = ["a", "e", "i", "o", "u"];
-    const words = str.split(" ");
-    const pigLatinWords = words.map(word => {
-        const firstLetter = word[0].toLowerCase();
-        const secondLetter = word[1] ? word[1].toLowerCase() : '';
-        if (vowels.includes(firstLetter)) {
-            return word + "way";
-        } else if (vowels.includes(secondLetter)) {
-            return word.slice(1) + firstLetter + "ay";
-        } else {
-            return word.slice(2) + word.slice(0, 2) + "ay";
-        }
-    });
-    return pigLatinWords.join(" ");
+function translatePigLatin(str) {
+ //var firstVowel = str.match(/aeiou/);
+ //var firstPosition = str.indexOf(firstVowel);
+ var firstPosition = findfirstVowelPosition(str);
+ if (firstPosition > 0){
+    return str.slice(firstPosition) + str.slice(0, firstPosition) + 'ou';
 }
-console.log(toPigLatin(" people are unique"));
+ return str + "you";
+}
+
+function findFirstVowelPosition(str) {
+    for (var i=0; i<str.length; i++) {
+        if ("aeiou".indexOf(str[i] !== -1))
+        return i;
+    }
+}
+
+
+translatePigLatin("people are unique")
